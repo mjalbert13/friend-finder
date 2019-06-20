2edcee85882f783c97bef384d452d0bad69d4ef1
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-06-20 13:42:39 -0400

    setting p api routs

--- app/routing/apiRouts.js
+++ app/routing/apiRouts.js
@@ -3,4 +3,37 @@ module.exports = function apiRoutes(app){
     var fs = require("fs");
     var path = require("path");
     var friends = require("./../data/friends");
+
+    app.get("/api/firiends", function(req,res){
+        return res.json(friends);
+    });
+
+    app.post("/api/friends", function(req,res){
+        
+        var totaldif;
+        var diffAray = [];
+        var newEntry = req.body;
+
+        for(var i = 0; i< friends.length; i++){
+            totaldif =0;
+            for(var k= 0; k < newEntry.scores.length; i++){
+                totaldif += Math.abs(friends[i].scores[k]-newEntry.scores[k]);
+            }
+            diffAray.push(totaldif)
+        }
+        var friendMatch = diffAray.indexOf(Math.min(...diffAray));
+
+        friends.push(newEntry);
+        console.log(newEntry);
+
+        fs.readFile(path.join(__dirname,"../data/friends.json"),"utf8",function(err,data){
+            if(err) throw err;
+            var json =JSON.parse(data);
+            json.push(newEntry);
+            fs.watchFile(path.join(__dirname, "../data/friends.json"),JSON.stringify(json,null,2),function(err){
+                if(err) throw err;
+            });
+        });
+        res.json(friends[friendMatch]);
+    })
 }
