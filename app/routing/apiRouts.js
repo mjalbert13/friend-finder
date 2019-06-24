318960b9adf4d5e342dedbf1b8537b106f8c110e
mjalbert13 <mjalbert13@gmail.com>
2019-06-24 17:31:29 -0400

    error in post request

--- app/routing/apiRouts.js
+++ app/routing/apiRouts.js
@@ -1,5 +1,6 @@
 var friends = require("../data/friends");
 var path = require("path");
+var friendArray = friends.friendslist;
 
 module.exports = function apiRoutes(app){
 
@@ -13,24 +14,50 @@ module.exports = function apiRoutes(app){
         console.log("User enterd "+JSON.stringify(newFriend));
         
         var friendScores = newFriend.scores;
-        var friendMatch ="";
-        var matchPhoto = "";
+        var differences = [];
+        var people = []
         
-        var totalDiff = 1000;
+        friendArray.forEach(function(person){
+            var matchScore = person.scores;
+            var totalDiff = 0;
 
-        for(var i =0; i< friends.length; i++){
-
-            var diff = 0;
-            for(var j = 0; j< friendScores; j++){
-                diff += Math.abs(friends[i].scores[j] - friendScores[j]);
+            for(var i = 0; i < matchScore.legth; i++){
+                totalDiff += Math.abs(matchScore[i] - friendScores[i]);
             }
-            if(diff < totalDiff){
-                totalDiff =diff;
-                friendMatch = friends[i].name;
-                matchPhoto = friends[i].photo;
-            }
-        }
-        friends.push(newFriend);
-        res.json({staus: "ok",friendMatch: friendMatch, matchPhoto: matchPhoto })
+            differences.push(totalDiff);
+            differences.sort();
+            bestScore = differences[0];
+            people.push({name: person.name, photo: person.photo, differences: totalDiff } )
+        });
+        var bestMatch = people.find(function(element){
+            return element.differences === bestScore;
+        });
+        friendArray.push(newFriend);
+        res.json(bestMatch);
     });
+
+    // app.post("/api/friends", function(req,res){
+    //     var userInput = req.body;
+    //     var friendScores = userInput.scores;
+    
+    //     var friendMatch ="";
+    //     var matchPhoto = "";
+    
+    //     var totalDiff = 1000;
+    //     for(var i =0; i< friends.length; i++){
+    
+    //         var diff = 0;
+    //         for(var j = 0; j< friendScores; j++){
+    //             diff += Math.abs(friends[i].scores[j] - friendScores[j]);
+    //         }
+    //         if(diff < totalDiff){
+    //             totalDiff =diff;
+    //             friendMatch = friends[i].name;
+    //             matchPhoto = friends[i].photo;
+    //         }
+    //     }
+    //     friends.push(newFriend);
+    //     res.json({staus: "ok",friendMatch: friendMatch, matchPhoto: matchPhoto })
+    
+    // });
 };
