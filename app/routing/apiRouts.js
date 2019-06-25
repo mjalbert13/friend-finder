5ef020c22eb34e047bab7bed3f5204647c86f17b
mjalbert13 <mjalbert13@gmail.com>
2019-06-25 11:38:53 -0400

    Updated the questions and logic

--- app/routing/apiRouts.js
+++ app/routing/apiRouts.js
@@ -24,28 +24,18 @@ module.exports = function(app){
         var convertScore =userScores.map(function(item){
             return parseInt(item,10);
         })
-        console.log("Name: "+userName);
-        console.log("User Score: "+userScores);
+        
         var sum = convertScore.reduce((a,b)=> a + b,0);
-        console.log("Sum of user score: "+sum);
-        console.log("Best match friend diff: "+newBest.diffBetween);
-        console.log("++++++++=========++++++++++");
-
+        
         for(var i=0; i<friends.length; i++){
             console.log(friends[i].name);
             finalDiff = 0;
-            console.log("Total diff: "+finalDiff);
+            //console.log("Total diff: "+finalDiff);
 
 
             var bfScores = friends[i].scores.reduce((a,b)=>a + b, 0);
             console.log("Total friend score: "+bfScores);
             finalDiff += Math.abs(sum - bfScores);
-
-            // for(var j = 0; j < friends[i].scores[j]; j++){
-
-            //     finalDiff += Math.abs(sum -parseInt(friends[i].scores[j]));
-            //     console.log(friends[i].scores[j]+ "Friend scores");
-            // };
 
             if(finalDiff <= newBest.diffBetween){
 
@@ -53,7 +43,7 @@ module.exports = function(app){
                 newBest.photo = friends[i].photo;
                 newBest.diffBetween = finalDiff;
             };
-            console.log(finalDiff +" Total Diff");
+            //console.log(finalDiff +" Total Diff");
         }
         console.log("your new best friend is "+newBest.name);
         friends.push(userData);
