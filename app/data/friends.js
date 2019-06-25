3ed3dbdba1af5d3531d00a63ec239b20420e9f25
mjalbert13 <mjalbert13@gmail.com>
2019-06-25 14:22:13 -0400

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,28 @@
 var friendslist =[
     {
-        name:"BatMan",
+        name:"Joe",
         photo:"https://cnet2.cbsistatic.com/img/PaxgyIO6vV-xdGx-qRceAhvEk8s=/fit-in/570x0/2017/06/10/d16c68ad-0f87-4218-8604-0ee6592ad4f1/batman-adam-west-getty-1.jpg",
         scores:[4,3,1,5,2,2,3,3,5,4]
     },
     {
-        name:"Archer",
+        name:"Sara",
         photo:"ghghghg",
         scores:[1,5,3,2,2,5,5,3,4,4]
     },
     {
-        name:"GOB Bluth",
+        name:"Greg",
         photo:"ghghghghjf",
         scores:[5,5,4,2,2,1,1,2,4,5]
+    },
+    {
+        name:"A plant",
+        photo:"",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name:"Gwen",
+        photo:"",
+        scores:[5,5,5,5,5,5,5,5,5,5]
     }
 ]
 module.exports = friendslist;
